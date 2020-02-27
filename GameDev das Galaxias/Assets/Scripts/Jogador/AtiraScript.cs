9e6a10b37bc7885ed4f43d353360b384af4f9b34
hnktech <danieltechio@hotmail.com>
2020-02-27 15:29:44 -0300

    archives

---
+++ GameDev das Galaxias/Assets/Scripts/Jogador/AtiraScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtiraScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _posicaoDeTiro;
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Atira();
+        }
+    }
+
+    private void Atira()
+    {
+        Instantiate(_laserPrefab, _posicaoDeTiro.position, _posicaoDeTiro.rotation);
+    }
+}
---
+++ GameDev das Galaxias/Assets/Scripts/Jogador/Movimento2dJogador.cs
@@ -0,0 +1,51 @@
+﻿using Arcaedion.DevDasGalaxias;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controle2D))]
+[RequireComponent(typeof(Animator))]
+public class Movimento2dJogador : MonoBehaviour
+{
+    [SerializeField]
+    private float _velocidade = 30f;
+
+    private Controle2D _controle;
+    private Animator _animator;
+    private Rigidbody2D _rigidBody;
+    private float _movimentoHorizontal;
+    private bool _pulando;
+
+    private void Awake()
+    {
+        _controle = GetComponent<Controle2D>();
+        _animator = GetComponent<Animator>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _movimentoHorizontal = Input.GetAxisRaw("Horizontal") * _velocidade;
+        if (Input.GetButtonDown("Jump"))
+            _pulando = true;
+
+        if (_controle.GetEstaNoChao())
+        {
+            _animator.SetFloat("Velocidade", Math.Abs(_rigidBody.velocity.x));
+            _animator.SetBool("Pulando", false);
+        }
+        else
+        {
+            _animator.SetBool("Pulando", true);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        _controle.Movimento(_movimentoHorizontal * Time.fixedDeltaTime, _pulando);
+        _pulando = false;
+    }
+}
